5559d15e3f346a0867525c95229cf42656be409c
kitska <xdxddd932@gmail.com>
2023-11-05 12:55:36 +0200

    test 1

---
+++ inc/uls.h
@@ -0,0 +1,19 @@
+
+#ifndef ULS_H
+
+#define ULS_H
+
+#include "../inc/libmx.h"
+#include <dirent.h>
+#include <errno.h>
+
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <time.h>
+
+
+
+#include <unistd.h>
+#endif
