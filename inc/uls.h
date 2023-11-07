1f30a325fd8a00abcb9285eb22d49fa57a1853e2
Nikita Finogenov <nikita.finogenov@gmail.com>
2023-11-07 18:52:57 +0200

    broken ls

--- inc/uls.h
+++ inc/uls.h
@@ -3,15 +3,29 @@
 
 #define ULS_H
 
-#include <libmx.h>
-#include <stdio.h>
-// #include <limits.h>
+#include <dirent.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/xattr.h>
+#include <sys/acl.h>
+#include <sys/ioctl.h>
+#include <pwd.h>
+#include <grp.h>
 #include <errno.h>
 #include <dirent.h>
-// #include <errno.h>
-// #include <sys/types.h>
-// #include <sys/stat.h>
-// #include <time.h>
+#include <string.h>
 #include <unistd.h>
+#include <libmx.h>
+
+
+typedef struct uls_stat_s {
+    unsigned long size;
+    unsigned long n_blks;
+    unsigned long blocks[32];
+} uls_stat_t;
+
+
+
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,28 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <uls.h>
 
-int main (int argc, char *argv[]) {
-    while (argc || argv)
-        break;
+int compare_names(const void *a, const void *b) {
+    return strcmp(*(const char **)a, *(const char **)b);
+}
 
+void print_multicolumn(const char *dirname) {
+    struct dirent *dir_entry;
+    char **files = NULL;
+    int num_files = 0;
 
-    struct dirent *entry;
-    DIR *dp;
-    if(argc == 1) 
-        dp = opendir(".");
-    else 
-        dp = opendir(argv[1]);
-    
-    if (dp == NULL) {
-        perror("Error opening directory");
-        return 1;
+    DIR *dir = opendir(dirname);
+    if (dir == NULL) {
+        perror("opendir");
+        exit(EXIT_FAILURE);
     }
-    while ((entry = readdir(dp))) {
-        if (entry->d_name[0] != '.')
-            printf("%s ", entry->d_name);
+
+    while ((dir_entry = readdir(dir)) != NULL) {
+        if (dir_entry->d_name[0] != '.') {
+            files = realloc(files, (num_files + 1) * sizeof(char *));
+            files[num_files] = strdup(dir_entry->d_name);
+            num_files++;
+        }
     }
-    printf("\n");
-    
-    closedir(dp);
+
+    closedir(dir);
+
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    int terminal_width = w.ws_col; // Get terminal width using ioctl
+
+    if (num_files > 0) {
+        qsort(files, num_files, sizeof(char *), compare_names);
+
+        int max_name_length = 0;
+        for (int i = 0; i < num_files; ++i) {
+            int name_length = strlen(files[i]);
+            if (name_length > max_name_length) {
+                max_name_length = name_length;
+            }
+        }
+
+        int num_columns = terminal_width / (max_name_length + 2); // 2 spaces between columns
+        int column_width = (terminal_width + 2) / num_columns - 2; // Calculate column width dynamically
+
+        int rows = (num_files + num_columns - 1) / num_columns;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < num_columns; ++j) {
+                int index = i + j * rows;
+                if (index < num_files) {
+                    printf("%-*s \t", column_width, files[index]);
+                }
+            }
+            printf("\n");
+        }
+
+        for (int i = 0; i < num_files; ++i) {
+            free(files[i]);
+        }
+        free(files);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    const char *dirname;
+    if (argc == 1) {
+        dirname = "."; // Use current directory if no argument is provided
+    } else if (argc == 2) {
+        dirname = argv[1];
+    } else {
+        fprintf(stderr, "Usage: %s [directory]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    print_multicolumn(dirname);
+
     return 0;
-    
 }
