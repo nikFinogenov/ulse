a84d47eb12dc3586f27e5096d2ccd591762f5359
Nikita Finogenov <nikita.finogenov@gmail.com>
2023-11-10 00:49:55 +0200

    100% working ls

--- src/main.c
+++ src/main.c
@@ -2,35 +2,40 @@
 #include <stdlib.h>
 #include <uls.h>
 
-int compare_names(const void *a, const void *b) {
-    return strcmp(*(const char **)a, *(const char **)b);
+static int compare_names(const void *a, const void *b) {
+    return mx_strcmp(*(const char **)a, *(const char **)b);
 }
-void print_tab_aligned_columns(int width, int tab_width) {
-    int tabs = (width + tab_width - 1) / tab_width;
 
-    for (int i = 0; i < tabs; i++) {
-        if (tab_width == 1) {
-            mx_printchar(' ');
-        } else {
-            mx_printchar('\t');
+static void custom_qsort(void *base, size_t num_elements, size_t element_size, int (*comparator)(const void *, const void *)) {
+    for (size_t i = 0; i < num_elements - 1; ++i) {
+        for (size_t j = i + 1; j < num_elements; ++j) {
+            void *element_i = (char *)base + i * element_size;
+            void *element_j = (char *)base + j * element_size;
+            if (comparator(element_i, element_j) > 0) {
+                char temp[element_size];
+                mx_memcpy(temp, element_i, element_size);
+                mx_memcpy(element_i, element_j, element_size);
+                mx_memcpy(element_j, temp, element_size);
+            }
         }
     }
 }
-void print_multicolumn(const char *dirname) {
+
+static void print_multicolumn(const char *dirname) {
     struct dirent *dir_entry;
     char **files = NULL;
     int num_files = 0;
 
     DIR *dir = opendir(dirname);
     if (dir == NULL) {
-        perror("opendir");
-        exit(EXIT_FAILURE);
+        mx_printerr("opendir");
+        exit(1);
     }
 
     while ((dir_entry = readdir(dir)) != NULL) {
         if (dir_entry->d_name[0] != '.') {
-            files = realloc(files, (num_files + 1) * sizeof(char *));
-            files[num_files] = strdup(dir_entry->d_name);
+            files = mx_realloc(files, (num_files + 1) * sizeof(char *));
+            files[num_files] = mx_strdup(dir_entry->d_name);
             num_files++;
         }
     }
@@ -45,11 +50,12 @@ void print_multicolumn(const char *dirname) {
     }
 
     if (num_files > 0) {
-        qsort(files, num_files, sizeof(char *), compare_names);
+        // qsort(files, num_files, sizeof(char *), compare_names);
+        custom_qsort(files, num_files, sizeof(char *), compare_names);
 
         int max_name_length = 0;
         for (int i = 0; i < num_files; ++i) {
-            int name_length = strlen(files[i]);
+            int name_length = mx_strlen(files[i]);
             if (name_length > max_name_length) {
                 max_name_length = name_length;
             }
@@ -74,7 +80,7 @@ void print_multicolumn(const char *dirname) {
                 }
                 index = index + rows;
             }
-            printf("\n");
+            mx_printchar('\n');
         }
 
         for (int i = 0; i < num_files; ++i) {
