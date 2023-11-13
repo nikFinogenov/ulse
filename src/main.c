183ab537505858885ef62132ea1b88c6aa7e971a
Nikita Finogenov <nikita.finogenov@gmail.com>
2023-11-14 00:13:23 +0200

    -R done, printf need to ubrat, -l working, combinations working. First tests done

--- src/main.c
+++ src/main.c
@@ -11,9 +11,9 @@ bool separate_flags(int argc, char *argv[]) {
     return false;
 }
 
-void recursive_flag(const char *path, s_flags_t *flags) {
+void recursive_flag(const char *path, FileEntry *file_entries, int count, s_flags_t *flags) {
     DIR *dir;
-    struct dirent *entry;
+    // struct dirent *entry;
 
     if (!(dir = opendir(path))) {
         perror("opendir");
@@ -22,42 +22,52 @@ void recursive_flag(const char *path, s_flags_t *flags) {
         return;
     }
     if (flags->l)
-        print_longlist(path, flags);
+        print_longlist(path, file_entries, count, flags);
     else if(flags->C)
         print_multicolumn(path, flags);
     else if (flags->one)
         print_perline(path, flags);
-    else 
+    else if(isatty(1))
         print_multicolumn(path, flags);
+    else
+        print_perline(path, flags);
 
     // Then, rewind the directory stream to start from the beginning
     closedir(dir);
-    dir = opendir(path);
+    // dir = opendir(path);
 
     // Now, process directories recursively
     // if (flags->one) {
-        while ((entry = readdir(dir)) != NULL) {
-            if (entry->d_name[0] == '.' || entry->d_type != DT_DIR) // Skip hidden files and non-directories
+        // while ((entry = readdir(dir)) != NULL) {
+        for(int i = 0; i < count; i++) {
+            if (file_entries[i].name[0] == '.' || file_entries[i].type != 'd')
                 continue;
 
-            printf("\n");
+
+            // printf("\n");
+            mx_printchar('\n');
             char *sub_path;
-            if (path[mx_strlen(path) - 1] == '/')
-                sub_path = mx_strjoin(path, entry->d_name);
-            else {
-                sub_path = mx_strjoin(mx_strjoin(path, "/"), entry->d_name);
-            }
+            // if (path[mx_strlen(path) - 1] == '/')
+            //     sub_path = mx_strjoin(path, file_entries[i].name);
+            // else {
+                sub_path = mx_strjoin(mx_strjoin(path, "/"), file_entries[i].name);
+            // }
             mx_printstr(path);
             mx_printchar('/');
-            mx_printstr(entry->d_name);
+            mx_printstr(file_entries[i].name);
             mx_printchar(':');
             mx_printchar('\n');
-            // snprintf(sub_path, sizeof(sub_path), "%s%s", path, entry->d_name);
-            recursive_flag(sub_path, flags);
+            // snprintf(sub_path, sizeof(sub_path), "%s%s", path, file_entries[i].name);
+            int inner_count;
+            FileEntry *inner_file_entries = fill_file_entries(sub_path, &inner_count, flags);
+            custom_qsort(inner_file_entries, inner_count, sizeof(FileEntry), compare_file_entries);
+            // printf("%s -> %d\n", sub_path, 0);
+            recursive_flag(sub_path, inner_file_entries, inner_count, flags);
+            free(inner_file_entries);
         }
     // }
 
-    closedir(dir);
+    // closedir(dir);
 }
 
 
@@ -68,11 +78,11 @@ int main(int argc, char *argv[]) {
     // bool with_flags = false;
     // bool with_dir = false;
     if (argc >= 2 && argv[1][0] == '-') { 
-        int count = parse_args(argc, argv, &flags);
+        int count_args = parse_args(argc, argv, &flags);
         // with_flags = true;
-        if (count == argc - 1) dirname = ".";
+        if (count_args == argc - 1) dirname = ".";
         else{
-            dirname = argv[count + 1];
+            dirname = argv[count_args + 1];
             // with_dir = true;
         }
     } 
@@ -84,22 +94,32 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Usage: %s [directory]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
+    int count_files;
+    FileEntry *file_entries = fill_file_entries(dirname, &count_files, &flags);
+    if (file_entries == NULL) {
+        return 0;
+    }
+
+    // Сортируем массив по именам
+    custom_qsort(file_entries, count_files, sizeof(FileEntry), compare_file_entries);
     // if (!with_flags){
     //     print_multicolumn(dirname, &flags);
     // }
     // else{
         if(flags.R)
-            recursive_flag(dirname, &flags);
+            recursive_flag(dirname, file_entries, count_files, &flags);
         else if (flags.l) {
-            print_longlist(dirname, &flags);
+            print_longlist(dirname, file_entries, count_files, &flags);
         }
         else if (flags.C)
             print_multicolumn(dirname, &flags);
         else if (flags.one) {
             print_perline(dirname, &flags);
         }
-        else
+        else if(isatty(1))
             print_multicolumn(dirname, &flags);
+        else
+            print_perline(dirname, &flags);
     // }
     return 0;
 }
---
+++ tests.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+
+# cat [filename] | od -A n -t x1     // for show hex
+home_dir=$HOME
+uls_output="$home_dir/uls_output.txt"
+ls_output="$home_dir/ls_output.txt"
+
+./uls > $uls_output
+ls > $ls_output
+
+diff_res=$(diff $uls_output $ls_output)
+if [ $? -eq 0 ]; then
+    echo "\"uls\" OK "
+else
+    echo "\"uls\" FALSE"
+    echo "$diff_res"
+fi
+
+./uls -R > $uls_output
+ls -R > $ls_output
+diff_res=$(diff $uls_output $ls_output)
+if [ $? -eq 0 ]; then
+    echo "\"uls -R\" OK "
+else
+    echo "\"uls -R\" FALSE"
+    echo "$diff_res"
+fi
+
+./uls -a > $uls_output
+ls -a > $ls_output
+diff_res=$(diff $uls_output $ls_output)
+if [ $? -eq 0 ]; then
+    echo "\"uls -a\" OK "
+else
+    echo "\"uls -a\" FALSE"
+    echo "$diff_res"
+fi
+
+./uls -l > $uls_output
+ls -l > $ls_output
+diff_res=$(diff $uls_output $ls_output)
+if [ $? -eq 0 ]; then
+    echo "\"uls -l\" OK "
+else
+    echo "\"uls -l\" FALSE"
+    echo "$diff_res"
+fi
+
+./uls -C > $uls_output
+ls -C > $ls_output
+diff_res=$(diff $uls_output $ls_output)
+if [ $? -eq 0 ]; then
+    echo "\"uls -C\" OK"
+else
+    echo "\"uls -C\" FALSE"
+    echo "$diff_res"
+fi
+
+./uls -1 > $uls_output
+ls -1 > $ls_output
+diff_res=$(diff $uls_output $ls_output)
+if [ $? -eq 0 ]; then
+    echo "\"uls -1\" OK "
+else
+    echo "\"uls -1\" FALSE"
+    echo "$diff_res"
+fi
+
+./uls -la > $uls_output
+ls -la > $ls_output
+diff_res=$(diff $uls_output $ls_output)
+if [ $? -eq 0 ]; then
+    echo "\"uls -la\" OK "
+else
+    echo "\"uls -la\" FALSE"
+    echo "$diff_res"
+fi
+
+./uls -al > $uls_output
+ls -al > $ls_output
+diff_res=$(diff $uls_output $ls_output)
+if [ $? -eq 0 ]; then
+    echo "\"uls -al\" OK "
+else
+    echo "\"uls -al\" FALSE"
+    echo "$diff_res"
+fi
+
+./uls -C1 > $uls_output
+ls -C1 > $ls_output
+diff_res=$(diff $uls_output $ls_output)
+if [ $? -eq 0 ]; then
+    echo "\"uls -C1\" OK "
+else
+    echo "\"uls -C1\" FALSE"
+    echo "$diff_res"
+fi
+
+./uls -1C > $uls_output
+ls -1C > $ls_output
+diff_res=$(diff $uls_output $ls_output)
+if [ $? -eq 0 ]; then
+    echo "\"uls -1C\" OK "
+else
+    echo "\"uls -1C\" FALSE"
+    echo "$diff_res"
+fi
+
+./uls -1C > $uls_output
+ls -1C > $ls_output
+diff_res=$(diff $uls_output $ls_output)
+if [ $? -eq 0 ]; then
+    echo "\"uls -1C\" OK "
+else
+    echo "\"uls -1C\" FALSE"
+    echo "$diff_res"
+fi
+
+./uls -1Cl > $uls_output
+ls -1Cl > $ls_output
+diff_res=$(diff $uls_output $ls_output)
+if [ $? -eq 0 ]; then
+    echo "\"uls -1Cl\" OK "
+else
+    echo "\"uls -1Cl\" FALSE"
+    echo "$diff_res"
+fi
+
+./uls -l1C > $uls_output
+ls -l1C > $ls_output
+diff_res=$(diff $uls_output $ls_output)
+if [ $? -eq 0 ]; then
+    echo "\"uls -l1C\" OK "
+else
+    echo "\"uls -l1C\" FALSE"
+    echo "$diff_res"
+fi
+
+# ./uls -Rl > $uls_output
+# ls -Rl > $ls_output
+# diff_res=$(diff $uls_output $ls_output)
+# if [ $? -eq 0 ]; then
+#     echo "\"uls -Rl\" OK "
+# else
+#     echo "\"uls -Rl\" FALSE"
+#     echo "$diff_res"
+# fi
+
+# ./uls -RaC > $uls_output
+# ls -RaC > $ls_output
+# diff_res=$(diff $uls_output $ls_output)
+# if [ $? -eq 0 ]; then
+#     echo "\"uls -RaC\" OK "
+# else
+#     echo "\"uls -RaC\" FALSE"
+#     echo "$diff_res"
+# fi
+
+# Очистить временные файлы
+rm $uls_output
+rm $ls_output
